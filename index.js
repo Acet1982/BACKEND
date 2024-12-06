3b0eb5891e8807b49d0022abe00745106e471925
BerrioA <Ingalejandroberrio@gmail.com>
2024-12-06 09:53:13 -0500

    correccion de ruta original del frontend

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://frontend-sigen.vercel.app/", "http://localhost:5173"],
+    origin: ["https://frontend-sigen.vercel.app", "http://localhost:5173"],
+    // methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );

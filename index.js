a9913edcbef109a121448442ba88a9b486c36979
BerrioA <Ingalejandroberrio@gmail.com>
2024-12-07 18:34:53 -0500

    ruta cambiada

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://frontend-sigen.vercel.app"],
+    origin: ["https://sigen-frontenf.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })

651ce0ff473d054c0f38980c01fa46139f50118b
BerrioA <Ingalejandroberrio@gmail.com>
2024-12-08 16:23:22 -0500

    fecha actualizada

--- utils/VerifyDate.js
+++ utils/VerifyDate.js
@@ -4,7 +4,7 @@ export const verifyDate = () => {
   const day = today.getDate();
 
   // Determinar el período basado en el día del mes
-  if (day >= 1 && day <= 5) {
+  if (day >= 1 && day <= 8) {
     return 1;
   } else if ([15, 16, 17, 18, 19, 28, 29, 30, 40].includes(day)) {
     return 2;
